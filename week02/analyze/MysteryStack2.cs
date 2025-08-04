95ae2ddc68bb424f27cb4a78e66612b084082b47
udodavid <davidudo00@gmail.com>
2025-08-04 18:30:58 +0100

    week 3

--- week02/analyze/MysteryStack2.cs
+++ week02/analyze/MysteryStack2.cs
@@ -1,49 +1,52 @@
-﻿public static class MysteryStack2 {
-    private static bool IsFloat(string text) {
+﻿public static class MysteryStack2
+{
+    private static bool IsFloat(string text)
+    {
         return float.TryParse(text, out _);
     }
 
-    public static float Run(string text) {
+    public static float EvaluatePostfix(string text)
+    {
         var stack = new Stack<float>();
-        foreach (var item in text.Split(' ')) {
-            if (item == "+" || item == "-" || item == "*" || item == "/") {
+
+        foreach (var item in text.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+        {
+            if (item == "+" || item == "-" || item == "*" || item == "/")
+            {
                 if (stack.Count < 2)
-                    throw new ApplicationException("Invalid Case 1!");
+                    throw new ApplicationException("Invalid Case 1: Not enough operands.");
 
-                var op2 = stack.Pop();
-                var op1 = stack.Pop();
-                float res;
-                if (item == "+") {
-                    res = op1 + op2;
-                }
-                else if (item == "-") {
-                    res = op1 - op2;
-                }
-                else if (item == "*") {
-                    res = op1 * op2;
-                }
-                else {
-                    if (op2 == 0)
-                        throw new ApplicationException("Invalid Case 2!");
-
-                    res = op1 / op2;
-                }
+                float op2 = stack.Pop();
+                float op1 = stack.Pop();
+                float res = item switch
+                {
+                    "+" => op1 + op2,
+                    "-" => op1 - op2,
+                    "*" => op1 * op2,
+                    "/" => op2 == 0 ? throw new ApplicationException("Invalid Case 2: Division by zero.") : op1 / op2,
+                    _ => throw new ApplicationException("Unknown operator.")
+                };
 
                 stack.Push(res);
             }
-            else if (IsFloat(item)) {
+            else if (IsFloat(item))
+            {
                 stack.Push(float.Parse(item));
             }
-            else if (item == "") {
-            }
-            else {
-                throw new ApplicationException("Invalid Case 3!");
+            else
+            {
+                throw new ApplicationException("Invalid Case 3: Invalid token.");
             }
         }
 
         if (stack.Count != 1)
-            throw new ApplicationException("Invalid Case 4!");
+            throw new ApplicationException("Invalid Case 4: Too many or too few operands.");
 
         return stack.Pop();
+
     }
+    
+    
 }
+
+
